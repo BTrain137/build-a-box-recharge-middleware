e6319383410be93104a70e038f344af123c68ef4
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-05-16 22:42:12 -0700

    Ahloha: add in extra route for allow all

--- routes/webhook/recharge.js
+++ routes/webhook/recharge.js
@@ -44,4 +44,43 @@ router.post("/order-created", async (req, res) => {
   res.sendStatus(200);
 });
 
+// /webhook/recharge/order-created--allow-all
+router.post("/order-created--allow-all", async (req, res) => {
+  const { order } = req.body;
+  const { tags, line_items, shopify_order_number } = order;
+  console.log("+++++++++++++++++++++++++++++++++++");
+  console.log("++++++++++++++Start++++++++++++++++");
+  console.log("shopify_order_number", shopify_order_number);
+  if (tags.includes("Subscription First Order")) {
+    const eligibleItems = [];
+    for (let i = 0; i < line_items.length; i++) {
+      const line_item = line_items[i];
+      const { subscription_id, unit_price } = line_item;
+      eligibleItems.push({
+        subscription_id,
+        unit_price,
+      });
+    }
+    if (eligibleItems.length === 0) {
+      return;
+    }
+
+    for (let j = 0; j < eligibleItems.length; j++) {
+      const item = eligibleItems[j];
+      const { subscription_id, unit_price } = item;
+      const results = await updateSubscriptionPrice(
+        subscription_id,
+        unit_price
+      );
+      const { product_title } = results.subscription;
+      console.log(`#${j}`, product_title);
+    }
+  } else {
+    console.log("Don't Process");
+  }
+  console.log("++++++++++++++END++++++++++++++++");
+  console.log("+++++++++++++++++++++++++++++++++");
+  res.sendStatus(200);
+});
+
 module.exports = router;
