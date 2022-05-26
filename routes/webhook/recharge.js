371935a950a870abbbf7b741bd07f438d64b7319
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-05-25 18:23:22 -0700

    Add route for all subscription

--- routes/webhook/recharge.js
+++ routes/webhook/recharge.js
@@ -44,6 +44,49 @@ router.post("/order-created", async (req, res) => {
   res.sendStatus(200);
 });
 
+// /webhook/recharge/order-created-all-subscription
+// Old Recharge style
+router.post("/order-created-all-subscription", async (req, res) => {
+  const { order } = req.body;
+  const { tags, line_items, shopify_order_number } = order;
+  console.log("+++++++++++++++++++++++++++++++++++");
+  console.log("++++++++++++++Start++++++++++++++++");
+  console.log("shopify_order_number", shopify_order_number);
+  if (tags.includes("Subscription First Order")) {
+    const eligibleItems = [];
+    for (let i = 0; i < line_items.length; i++) {
+      const line_item = line_items[i];
+      const { subscription_id, unit_price, properties } = line_item;
+      const isSubscriptionItem = properties.some(({ name }) => name == "shipping_interval_unit_type");
+      if (isSubscriptionItem) {
+        eligibleItems.push({
+          subscription_id,
+          unit_price,
+        });
+      }
+    }
+    if (eligibleItems.length === 0) {
+      return;
+    }
+
+    for (let j = 0; j < eligibleItems.length; j++) {
+      const item = eligibleItems[j];
+      const { subscription_id, unit_price } = item;
+      const results = await updateSubscriptionPrice(
+        subscription_id,
+        unit_price
+      );
+      const { product_title } = results.subscription;
+      console.log(`#${j}`, product_title);
+    }
+  } else {
+    console.log("Don't Process");
+  }
+  console.log("++++++++++++++END++++++++++++++++");
+  console.log("+++++++++++++++++++++++++++++++++");
+  res.sendStatus(200);
+});
+
 // /webhook/recharge/order-created--allow-all
 router.post("/order-created--allow-all", async (req, res) => {
   const { order } = req.body;
